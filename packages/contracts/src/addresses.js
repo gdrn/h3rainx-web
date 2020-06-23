077ecf934894b0faca5b0f123036521e7e25af03
gdrn <gdrn@protonmail.com>
2020-06-22 19:13:21 -0700

    Add goldenrain abi and address

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,7 +1,7 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
+  goldenRain: "0xF56ccB441153119A373F0aD4909B8f50121a33bd",
 };
 
 export default addresses;
