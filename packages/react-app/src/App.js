427fd338de3796f44faa7ccfec725759cd24c888
gdrn <gdrn@protonmail.com>
2020-06-23 02:06:18 -0700

    Fix isactive

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -87,7 +87,7 @@ function App() {
   },[])
 
   const time = Date.UTC(2020,5,24,7,0,0,0) // goldenrain acctivation
-  const isActive = true
+  let isActive = false
   if (Date.now() > time )
     isActive = true
 
