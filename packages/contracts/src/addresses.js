266e9d369dc16faf41885042112cd28bc533f2f2
gdrn <gdrn@protonmail.com>
2020-06-23 13:30:40 -0700

    Update yarn and addresses

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,6 +2,7 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   goldenRain: "0x438f8795f8dFb195140eEB5eaD8b0e794Bf33Ee8",
+  rain: "0x61cDb66e56FAD942a7b5cE3F419FfE9375E31075"
 };
 
 export default addresses;
